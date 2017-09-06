c76288dbf0999faa29df86d28aef1a4ae126cf4d
Williammer <supervegeter@gmail.com>
2017-09-06 08:23:31 +0800

    Simple refactoring on the lozad.js to be more readable.

--- src/lozad.js
+++ src/lozad.js
@@ -1,12 +1,44 @@
 ;(function() {
 
-    const _Lozad = function(config) {
+    function getElements() {
+        return document.querySelectorAll(this.config.selector);
+    }
+
+    function loadAll() {
+        const elements = getElements.call(this);
+        elements.forEach(function (element) {
+            if (!isLoaded(element)) {
+                this.load(element);
+                markAsLoaded(element);
+            }
+        }.bind(this));
+    }
+
+    function markAsLoaded(el) {
+        el.dataset.loaded = true;
+    }
+
+    function onIntersection(entries) {
+        entries.forEach(function (entry) {
+            if (entry.intersectionRatio > 0) {
+                this.observer.unobserve(entry.target);
+                this.load(entry.target);
+                markAsLoaded(entry.target);
+            }
+        }.bind(this));
+    }
+
+    function isLoaded(el) {
+        return (el.dataset.loaded === "true");
+    }
+
+    function _Lozad(config) {
         this.config = config || {};
         this.config.selector = this.config.selector || '.lozad';
         this.config.rootMargin = this.config.rootMargin || '0px';
         this.config.threshold = this.config.threshold || 0;
         this.activate();
-    };
+    }
 
     _Lozad.prototype.activate = function() {
         if (!window.IntersectionObserver) {
@@ -16,49 +48,17 @@
                 this.observer.disconnect();
             }
             this.observer = new IntersectionObserver(onIntersection.bind(this), this.config);
-            var elements = getElements.call(this);
-            for (var i = 0; i < elements.length; i++) {
-                if (!isLoaded(elements[i])) {
-                    this.observer.observe(elements[i]);
+            const elements = getElements.call(this);
+            elements.forEach(function (element) {
+                if (!isLoaded(element)) {
+                    this.observer.observe(element);
                 }
-            }
-        }
-    };
-
-    const getElements = function() {
-        return document.querySelectorAll(this.config.selector);
-    };
-
-    const loadAll = function() {
-        var elements = getElements.call(this);
-        for (var i = 0; i < elements.length; i++) {
-            if (!isLoaded(elements[i])) {
-                this.load(elements[i]);
-                markAsLoaded(elements[i]);
-            }
+            }.bind(this));
         }
     };
 
     _Lozad.prototype.load = function(el) {
         el.src = el.dataset.src;
-    };
-
-    const markAsLoaded = function(el) {
-        el.dataset.loaded = true;
-    };
-
-    const onIntersection = function(entries) {
-        for (var i = 0; i < entries.length; i++) {
-            if (entries[i].intersectionRatio > 0) {
-                this.observer.unobserve(entries[i].target);
-                this.load(entries[i].target);
-                markAsLoaded(entries[i].target);
-            }
-        }
-    };
-
-    const isLoaded = function(el) {
-        return (el.dataset.loaded === "true");
     };
 
     // open to the world.
